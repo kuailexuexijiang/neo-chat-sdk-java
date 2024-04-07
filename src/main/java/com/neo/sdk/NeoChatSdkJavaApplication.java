564de3a24c9bca81a8d8559c566c96365e4e5afb
zhangxin <zhang_xin@tom.com>
2024-04-07 13:40:58 +0800

    init

--- src/main/java/com/neo/sdk/NeoChatSdkJavaApplication.java
+++ src/main/java/com/neo/sdk/NeoChatSdkJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NeoChatSdkJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NeoChatSdkJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NeoChatSdkJavaApplication.class, args);
+    }
 
 }
