6302b59dbd546e3de7b76ea40688f5864f540e4b
zhangxin <zhang_xin@tom.com>
2024-04-07 13:40:27 +0800

    Initial commit

---
+++ src/main/java/com/neo/sdk/NeoChatSdkJavaApplication.java
@@ -0,0 +1,13 @@
+package com.neo.sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoChatSdkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoChatSdkJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo/sdk/NeoChatSdkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoChatSdkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
